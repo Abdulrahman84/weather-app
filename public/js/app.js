29ec02bd885d5427b3a3e5ffe14de7cac4c2eb88
unknown <you@example.com>
2020-04-25 16:10:22 +0300

    current location update

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message1')
 const message2 = document.querySelector('#message2')
+const currentLocation = document.querySelector('#current-location')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -18,6 +19,32 @@ weatherForm.addEventListener('submit', (e) => {
                 message2.textContent = data.forecast
                 search.value = ''
             }
+        })
+    })
+})
+
+currentLocation.addEventListener('click', () => {
+
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        fetch('/weather?address=' + position.coords.latitude + ',' + position.coords.longitude).then((res) => {
+            
+            res.json().then((data) => {
+                if (data.error) {
+                    message1.textContent = data.error
+                } else {
+                    message1.textContent = data.location
+                    message2.textContent = data.forecast
+                    search.value = ''
+                }
+            })
         })
     })
 })
